ee4fb5d4342c8360c2a92b5c0b74d40ef558183f
zfudge <zacheryfudge@gmail.com>
2018-01-19 04:32:29 -0700

    json parsing, quote contents accessible

--- quote.js
+++ quote.js
@@ -1,14 +1,17 @@
 const page = {
+  source: document.getElementById('source'),
   quote: document.getElementById('quote'),
+  person: document.getElementById('personName'),
   newQuoteBtn: document.getElementById('newQuote'),
+  tweet: document.getElementById('tweet'),
   colors: ['rgba(255,0,33,0.65)', 'rgba(69,227,48,0.65)', 'rgba(48,182,227,0.65)', 'rgba(139,21,235,0.65)']
 }
-page.newQuoteBtn.onclick = update;
+page.newQuoteBtn.onclick = getNewQuoteAndColor;
 
 function colorShift() {
   const newColor = page.colors[Math.floor(Math.random() * page.colors.length)];
   document.body.style.backgroundColor = newColor;
-  document.getElementById('source').style.color = newColor;
+  page.person.style.color = newColor;
   page.quote.style.color = newColor;
   Array.from(document.getElementsByTagName('button')).forEach(function(cur) {
     cur.style.backgroundColor = newColor;
@@ -16,26 +19,27 @@ function colorShift() {
 }
 colorShift();
 
-function update() {
+function getNewQuoteAndColor() {
+  const quote = xhr[Math.floor(Math.random() * xhr.length)];
+  const text = quote.content.replace(/<p>/ig, '');
+  const person = `- ${quote.title}`;
+  page.quote.innerHTML = text;
+  page.person.innerHTML = person; 
+  tweetAssign(text, person);
   colorShift();
-  page.quote.innerHTML = quotes[Math.floor(Math.random() * xhr.length)].content;
 }
 
+function tweetAssign(quote,name) {
+  page.tweet.children[0].href = `https://twitter.com/intent/tweet?text=${quote}${name}`;
+}
 
-const xhr = new XMLHttpRequest();
-const url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30";
+let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=20");
-xhr.send();
-
-let quotes;
-function testResponse() {
-  try {
-    quotes = [].slice.call(JSON.parse(xhr.response));
-  } catch(e) {
-    console.log(e);
-    alert("x(");
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    xhr = [].slice.call(JSON.parse(xhr.response));
   }
 }
 
-testResponse();
+xhr.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=20");
+xhr.send();
